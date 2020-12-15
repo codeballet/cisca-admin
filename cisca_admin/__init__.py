8f204a557496c98608f5f7dd4682aa00e9c9ef1c
Johan <stjernholm@msn.com>
2020-12-15 19:17:46 +0800

    Refac: preloaded countries table for create view

--- cisca_admin/__init__.py
+++ cisca_admin/__init__.py
@@ -49,7 +49,7 @@ def create_app(test_config=None):
 
     init_db()
 
-    # Command line apps
+    # Click Command line apps
     from . import click_countries
     app.cli.add_command(click_countries.countries)
 
